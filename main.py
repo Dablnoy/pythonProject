32ec10bc0cc28be7ec4297869546d7a3b604d897
Dablnoy <go.8761@mail.ru>
2022-09-20 23:03:10 +0600

    2 task

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-a=int(input("Insert a: "))
-for i in range(a):
-    b=((i+1)**3)
-    print("Number is:",i+1," and cube if the ",i+1," is ",b)
+# task 1
+# a=int(input("Insert a: "))
+# for i in range(a):
+#     b=((i+1)**3)
+#     print("Number is:",i+1," and cube if the ",i+1," is ",b)
+
+# task 2
+
+# a=list(map(int,input().split()))
+# a.reverse()
+# for i in range(len(a)):
+#     print(a[i])
+
+
+
