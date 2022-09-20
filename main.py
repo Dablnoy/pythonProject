c2f0b9a415d4a5db3c2984e8591215b861186c92
Dablnoy <go.8761@mail.ru>
2022-09-20 23:41:27 +0600

    5 task

--- main.py
+++ main.py
@@ -25,3 +25,15 @@
 #
 # print(sum)
 
+# task 5
+# a=int(input())
+# temp=a
+# b=0
+# while a>0:
+#     c=a%10
+#     a=a//10
+#     b=b*10+c
+# if(temp==b):
+#     print("palindrome")
+# else:
+#     print("No palindrome")
